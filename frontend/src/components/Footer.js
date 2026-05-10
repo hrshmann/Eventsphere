32769d11c7c29aa299aa32606b9bb6ffa3159a8f
hrshmann <harsh.info007@gmail.com>
2026-05-11 02:25:15 +0530

    Create Footer.js

---
+++ frontend/src/components/Footer.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <footer
+      style={{
+        textAlign: "center",
+        padding: "15px",
+        backgroundColor: "#111",
+        color: "#fff",
+        marginTop: "20px",
+        fontSize: "14px",
+      }}
+    >
+      Developed by Hazik Muzafar
+    </footer>
+  );
+}
+
+export default Footer;
